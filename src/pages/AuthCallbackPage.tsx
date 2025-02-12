ba45c3425549b13490c833c82f94fa5bd2d6db36
Trevor <trevormngwenya@gmail.com>
2025-02-12 08:07:24 +0200

    Add auth token verification

--- src/pages/AuthCallbackPage.tsx
+++ src/pages/AuthCallbackPage.tsx
@@ -12,7 +12,7 @@ const AuthCallbackPage = () => {
 
     useEffect(() => {
         if (user?.sub, user?.email && !hasCreatedUser.current) {
-            createUser({ email:user.email, auth0Id:String(user.sub) });
+            createUser({ email:String(user?.email), auth0Id:String(user?.sub) });
             hasCreatedUser.current = true;
         }
         navigate("/");
