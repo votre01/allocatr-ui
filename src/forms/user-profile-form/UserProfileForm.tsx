81929371116b369b375fb149a4b6a59f71ef27b2
Trevor <trevormngwenya@gmail.com>
2025-03-17 07:07:28 +0200

    Setup user profile form

--- src/forms/user-profile-form/UserProfileForm.tsx
+++ src/forms/user-profile-form/UserProfileForm.tsx
@@ -1,7 +1,20 @@
 import { z } from "zod";
 
 const formSchema = z.object({
-    firstname: z.string().min(1, "First name is required"),
-    
+    first_name: z.string().min(1, "First name is required"),
+    last_name: z.string().min(1, "Last name is required"),
+    birthdate: z.string().min(1, "Birth date is required"),
+    gender: z.string().min(1, "Gender is required"),
+    phone: z.string().min(1, "Phone is required"),
+    id_number: z.string().min(1, "ID number is required"),
+    profile_picture: z.string().optional(),
+    bio: z.string().min(1, "Biography is required"),
+    is_company: z.boolean(),
+    company_name: z.string().optional(),
+    company_registration: z.string().optional(),
+    accreditation: z.string().optional(),
+    city: z.string().min(1, "City is required"),
+    country: z.string().min(1, "Country is required"),    
 });
 
+type UserFormData = z.infer<typeof formSchema>;
